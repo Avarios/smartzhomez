95fe45b15233c779214b4857c0304c8e1862545c
Pawel Warmuth <warmuth.pawel@gmail.com>
2023-10-07 10:03:29 +0200

    Added battery route into main

--- IOBrokerAPI/src/main.rs
+++ IOBrokerAPI/src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate actix_web;
-
+mod e3dc;
 use std::{env, io};
 use clap::Parser;
 use actix_web::{middleware, App, HttpServer};
@@ -24,13 +24,7 @@ async fn main() -> io::Result<()> {
             // enable logger - always register actix-web Logger middleware last
             .wrap(middleware::Logger::default())
             // register HTTP requests handlers
-            .service(tweet::list)
-            .service(tweet::get)
-            .service(tweet::create)
-            .service(tweet::delete)
-            .service(like::list)
-            .service(like::plus_one)
-            .service(like::minus_one)
+            .service(e3dc::get_battery)
     })
     .bind("0.0.0.0:9090")?
     .run()
